672538677cc622cd852da054306f8144445a25fb
mt5225 <mt5225@gmail.com>
2015-09-07 19:29:52 -0700

    update unity3d

--- js/umodel.js
+++ js/umodel.js
@@ -3,7 +3,7 @@
  */
 var uModel = uModel || {};
 var external = null;
-uModel.scene = "ubuilder-0.4.594.unity3d";
+uModel.scene = "ubuilder-0.7.621.unity3d";
 uModel.loadJson = "";
 uModel.param = "";
 uModel.web = {};
